86e55fed1fa929aae4be3a9bc2db9e2c3da5f658
Juanri Dwight Tambalean <107243910+JuanriTambalean@users.noreply.github.com>
2024-05-16 14:12:54 +0800

    Add files via upload

---
+++ member.js
@@ -0,0 +1,34 @@
+const members = [
+    {
+        id: 1,
+        nim: '105022110004',
+        nama: 'Juanri Tambalean',
+        nomor_telp: '082233445566'
+    },
+    {
+      id: 2,
+      nim: '105022110039',
+      nama: 'Kevin Clief',
+      nomor_telp: '085157809772'
+    },
+    {
+      id: 3,
+      nim: '105022110036',
+      nama: 'Fransditho Enir',
+      nomor_telp: '08979578331'
+    },
+    {
+      id: 4,
+      nim: '105022020003',
+      nama: 'Falentino Tombeng',
+      nomor_telp: '085395102059'
+    },
+    {
+        id: 5,
+        nim: '105022210039',
+        nama: 'Rionaldo Siaman',
+        nomor_telp: '08114316501'
+    },
+  ];
+  
+  module.exports = members;
